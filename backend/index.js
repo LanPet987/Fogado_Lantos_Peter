27bd3732c08f9a03fe6110eaaef676724c4ab478
LanPet987 <115153511+LanPet987@users.noreply.github.com>
2025-10-06 11:29:54 +0200

    alap

--- backend/index.js
+++ backend/index.js
@@ -8,3 +8,27 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'fogado',
+    port: 3306
+});
+
+app.get('/teszt', (req, res) => {
+    sql = "SELECT 1 + 1 AS result";
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`A szerver fut a ${port} porton`);
+});
